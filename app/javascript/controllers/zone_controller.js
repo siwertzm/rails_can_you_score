fd6f4ce8a37ea739b7820ae9aa261deeafb44c13
marius siwertz <siwertzm@gmail.com>
2023-11-28 17:01:21 +0100

    training done

--- app/javascript/controllers/zone_controller.js
+++ app/javascript/controllers/zone_controller.js
@@ -2,7 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="zone"
 export default class extends Controller {
+  static targets = ["clickableZone", "zoneId", "close", "modal"]
+
   connect() {
-    
+    console.log("Zone controller connected");
+    console.log(this.closeTarget);
+    this.clickableZoneTargets.forEach(element => {
+      element.addEventListener("click", () => {
+        this.zoneIdTarget.value = element.getAttribute("data-zone-id");
+        this.openModal();
+      });
+    });
+
+    this.closeTarget.addEventListener("click", () => {
+      this.modalTarget.style.display = "none";
+    });
+  }
+
+  openModal() {
+    this.modalTarget.style.display = "block";
   }
 }
