a4f8d3d7f0415ee1b400d65d1b37f4874f36d347
Raphael Wais <raphael.wais@gmail.com>
2023-11-28 12:29:18 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Créé les 11 zones, 10 users avec 5 sessions chacuns...'
+
+zones = ["P", "FT", "TF", "RZ", "TRZ", "RE", "TRE", "LZ", "TLZ", "LE", "TLE"]
+compteur = 0
+11.times do
+  zone = Zone.create(
+    name: zones[compteur]
+  )
+  zone.save!
+  compteur += 1
+end
+
+10.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: "azerty"
+  )
+  user.save!
+
+  5.times do
+    training = Training.create(
+      shot_total: 20,
+      shot_made: rand(21),
+      user_id: User.find_by(username: user.username).id,
+      zone_id: Zone.find_by(name: zones[rand(1..11)])
+    )
+    training.save!
+  end
+end
+puts 'Finished!'
