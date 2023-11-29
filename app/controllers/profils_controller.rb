db86a202371e17fcbf502a31a083c91abe03d3d6
Johan FILONNEAU <filonovjohan@gmail.com>
2023-11-29 12:32:04 +0100

    leaderboard

--- app/controllers/profils_controller.rb
+++ app/controllers/profils_controller.rb
@@ -1,11 +1,5 @@
 class ProfilsController < ApplicationController
-
   def show
     @profil = User.find_by(id: params[:id])
   end
-
-  def leaderboard
-    @top_users = User.order(shooting_efficiency: :desc).limit(10)
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "pages#home"
   resources :trainings
   resources :profils
+  resources :leaderboards, only: [:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
