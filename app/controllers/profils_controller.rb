8bc5911551983ca59e25e4271c97379f90bc7743
Johan FILONNEAU <filonovjohan@gmail.com>
2023-11-29 11:18:51 +0100

    12312312312

--- app/controllers/profils_controller.rb
+++ app/controllers/profils_controller.rb
@@ -1,5 +1,11 @@
 class ProfilsController < ApplicationController
+
   def show
-    @profil = User.find(params[:id])
+    @profil = User.find_by(id: params[:id])
   end
+
+  def leaderboard
+    @top_users = User.order(shooting_efficiency: :desc).limit(10)
+  end
+
 end
