fb2d15efb80bea99e51d09d27aaacf0e02f35aed
Johan FILONNEAU <filonovjohan@gmail.com>
2023-11-30 14:26:39 +0100

    front login signup password and stats page, new route for it, display with js trainings, fixed 3 best sessions in profile show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,10 @@ class PagesController < ApplicationController
 
     @trainings = Training.all
   end
+
+  def stats
+    @user = current_user
+    @best_trainings = @user.trainings.to_a.sort_by { |training| -training.shooting_efficiency }
+    # Add more stats as needed
+  end
 end
