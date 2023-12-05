71ab8804ad4a527fddce20a4bb7da382e6ab9925
Johan FILONNEAU <filonovjohan@gmail.com>
2023-12-05 15:22:40 +0100

    stats stats stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :set_user_profile, only: :stats
+
   def home
     respond_to do |format|
       format.html
@@ -10,17 +12,65 @@ class PagesController < ApplicationController
   end
 
   def stats
+    set_best_trainings
+    set_latest_trainings_for_table
+    set_followers_trainings_data
+    set_followers_trainings
+  end
+
+  def set_user_profile
     @profil = current_user
+    redirect_to root_path, alert: 'User not found' unless @profil.present?
+  end
+
+  def set_best_trainings
     @best_trainings = @profil.trainings.to_a.sort_by { |training| -training.shooting_efficiency }
-    if params[:zone_id]
-      @latest_trainings_for_table = @profil.trainings
-        .where(zone_id: params[:zone_id])
-        .select("*, CASE WHEN shot_total > 0 THEN (shot_made::float / shot_total) * 100 ELSE 0 END AS shooting_efficiency")
-        .order(created_at: :desc)
+  end
+
+  def set_latest_trainings_for_table
+    zone_id = params[:zone_id]
+
+    @latest_trainings_for_table = if zone_id
+      @profil.trainings
+             .where(zone_id: zone_id)
+             .select("*, CASE WHEN shot_total > 0 THEN (shot_made::float / shot_total) * 100 ELSE 0 END AS shooting_efficiency")
+             .order(created_at: :desc)
     else
-      @latest_trainings_for_table = @profil.trainings
-        .select("*, CASE WHEN shot_total > 0 THEN (shot_made::float / shot_total) * 100 ELSE 0 END AS shooting_efficiency")
-        .order(created_at: :desc)
+      @profil.trainings
+             .select("*, CASE WHEN shot_total > 0 THEN (shot_made::float / shot_total) * 100 ELSE 0 END AS shooting_efficiency")
+             .order(created_at: :desc)
     end
+  end
+
+  def set_followers_trainings_data
+    @profil.followees ||= []
+
+    @followers_trainings_data = @profil.followees.map do |follower|
+      {
+        label: follower.username,
+        trainings: set_follower_trainings(follower)
+      }
+    end
+  end
+
+  def set_follower_trainings(follower)
+    zone_id = params[:zone_id]
+
+    trainings = if zone_id
+      follower.trainings
+              .where(zone_id: zone_id)
+              .select("*, CASE WHEN shot_total > 0 THEN (shot_made::float / shot_total) * 100 ELSE 0 END AS shooting_efficiency")
+              .order(created_at: :desc)
+    else
+      follower.trainings
+              .select("*, CASE WHEN shot_total > 0 THEN (shot_made::float / shot_total) * 100 ELSE 0 END AS shooting_efficiency")
+              .order(created_at: :desc)
+    end
+
+    trainings.compact || []
+  end
+
+  def set_followers_trainings
+    @followers_trainings = @profil.followees.flat_map { |follower| set_follower_trainings(follower) }
   end
 end
