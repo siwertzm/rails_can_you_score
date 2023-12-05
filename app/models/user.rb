ee8977916b028fc00579f8b233da6c9113b3dfa5
gregory rixens <greg74290@yahoo.fr>
2023-12-05 10:36:49 +0100

    editpic

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
+  has_one_attached :banner
 
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followees, through: :followed_users, source: :followed
