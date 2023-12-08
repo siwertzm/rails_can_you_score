18a47fd925ac836aed9a2cb843879e716abe0343
marius siwertz <siwertzm@gmail.com>
2023-12-08 11:20:00 +0100

    obsetion

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,18 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = [ "toggleable", "tablemoi", "tablemoi2", "toggleable2", "titre" ]
+  static targets = [ "toggleable", "tablemoi", "tablemoi2", "toggleable2", "titre", "titre2" ]
   connect() {
     this.toggleableTarget.addEventListener("click", () => {
       this.tablemoiTarget.classList.toggle("d-none");
-      this.titreTarget.classList.toggle("pb-5");
-      this.titreTarget.classList.toggle("mb-5");
+      this.tablemoiTarget.classList.toggle("pb-5");
+      this.tablemoiTarget.classList.toggle("mb-5");
     });
 
     this.toggleable2Target.addEventListener("click", () => {
       this.tablemoi2Target.classList.toggle("d-none");
-      this.titreTarget.classList.toggle("pb-5");
-      this.titreTarget.classList.toggle("mb-5");
+      this.titre2Target.classList.toggle("pb-5");
+      this.titre2Target.classList.toggle("mb-5");
     });
   }
 }
+s
